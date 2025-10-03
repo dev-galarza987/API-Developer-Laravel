0b734c2f8de10fb709edaa8fd2b5de3100706f7a
Victor Hugo Galarza Jimenez <dev.galarza@gmail.com>
2025-10-03 17:52:38 -0400

    refactor(api): modify the route GET /user using the DeveloperController.index()

--- routes/api.php
+++ routes/api.php
@@ -1,9 +1,7 @@
 <?php
 
-use App\Models\api\Developer;
+use App\Http\Controllers\DeveloperController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return "User route of API";
-});
+Route::get('/user', [DeveloperController::class, 'index']);
